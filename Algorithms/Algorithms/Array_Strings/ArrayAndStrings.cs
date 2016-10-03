4c580a141551b9c3f0b502be1971ae35c89e4629
Dwivedi Vishal Sridhar Arya <v-dwarya@microsoft.com>
2016-10-03 08:27:03 +0530

    Algo_If String has all unique Characters

--- Algorithms/Algorithms/Array_Strings/ArrayAndStrings.cs
+++ Algorithms/Algorithms/Array_Strings/ArrayAndStrings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,61 @@ namespace Algorithms.Array_Strings
 {
     public class ArrayAndStrings
     {
-        
+        public static void UniqueCharCount_1(string word)
+        {
+            if (!string.IsNullOrEmpty(word))
+            {
+                Dictionary<char, int> tempDic = new Dictionary<char, int>();
+                bool uniqueCheck = true;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (tempDic.ContainsKey(word[i]))
+                    {
+                        tempDic[word[i]]++;
+                    }
+                    else
+                        tempDic.Add(word[i], 1);
+                }
+                foreach (var item in tempDic)
+                {
+                    if (item.Value > 1)
+                    {
+                        uniqueCheck = false;
+                        break;
+                    }
+                }
+                if(uniqueCheck)
+                    Console.WriteLine($"Given String {word} has unique characters");
+                else
+                    Console.WriteLine($"Given String {word} has repeating characters");
+            }
+            else
+                Console.WriteLine("Word provided is either empty or null");
+        }
+
+        public static void UniqueCharCount_2(string word)
+        {
+            if (!string.IsNullOrEmpty(word))
+            {
+                ConcurrentDictionary<char, int> tempDic = new ConcurrentDictionary<char, int>();
+                bool uniqueCheck = true;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    tempDic.AddOrUpdate(word[i], 1, (s, n) => n + 1);
+                }
+                foreach (var item in tempDic)
+                {
+                    if (item.Value > 1)
+                    {
+                        uniqueCheck = false;
+                        break;
+                    }
+                }
+                if (uniqueCheck)
+                    Console.WriteLine($"Given String {word} has unique characters");
+                else
+                    Console.WriteLine($"Given String {word} has repeating characters");
+            }
+        }
     }
 }
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithms.Array_Strings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            ArrayAndStrings.UniqueCharCount_2("abcdefghijklmnopqrstuyuiojgjnl");
+            Console.ReadLine();
         }
     }
 }
