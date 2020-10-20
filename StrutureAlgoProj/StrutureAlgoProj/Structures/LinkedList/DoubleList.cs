81ca63418254fb81775967d807eee9c7a1f60018
Dwivedi Vishal Sridhar Arya <vishary@amazon.com>
2020-10-20 13:14:44 +0530

    Adding folder structure and dummy files.

---
+++ StrutureAlgoProj/StrutureAlgoProj/Structures/LinkedList/DoubleList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrutureAlgoProj.Structures.LinkedList
+{
+    public class DoubleList
+    {
+        
+    }
+}
