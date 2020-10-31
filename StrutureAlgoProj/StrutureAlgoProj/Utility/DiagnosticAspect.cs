9e77d5640d47f165b1edfb78f855d8eaa5e08ac5
Dwivedi Vishal Sridhar Arya <vishary@amazon.com>
2020-10-31 17:28:25 +0530

    Adding Diagnostic Attribute to measure performance.

---
+++ StrutureAlgoProj/StrutureAlgoProj/Utility/DiagnosticAspect.cs
@@ -0,0 +1,32 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrutureAlgoProj.Utility
+{
+    [Serializable]
+    public class DiagnosticAspect: OnMethodBoundaryAspect
+    {
+        [NonSerialized]
+        public Stopwatch _sw;
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            //Console.WriteLine("The {0} method has been entered.", args.Method.Name);
+            _sw = new Stopwatch();
+            _sw.Start();
+        }
+
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            _sw.Stop();
+            Console.WriteLine($"The Execution took : {_sw.ElapsedMilliseconds} Milliseconds");
+        }
+
+    }
+}
